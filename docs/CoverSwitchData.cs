7974288f5359c0d8789911edd83c5dc3beb36557
Asnxthaony <asnxthaony@outlook.com>
2023-05-23 23:35:29 +0800

    add some magic definitions

---
+++ docs/CoverSwitchData.cs
@@ -0,0 +1,64 @@
+namespace MoleMole
+{
+    public static class CoverSwitchData
+    {
+        public const int DEFAULT = 0;
+        public const int GACHA = 1;
+        public const int MALL = 2;
+        public const int BATTLE_PASS = 3;
+        public const int BULLETIN = 4;
+        public const int MAIL = 5;
+        public const int TIME = 6;
+        public const int COMMUNITY = 7;
+        public const int HANDBOOK = 8;
+        public const int FEEDBACK = 9;
+        public const int QUEST = 10;
+        public const int MAP = 11;
+        public const int TEAM = 12;
+        public const int FRIENDS = 13;
+        public const int AVATAR_LIST = 14;
+        public const int CHARACTER = 15;
+        public const int ACTIVITY = 16;
+        public const int Multiplayer = 17;
+        public const int RECHARGE_CARD = 18;
+        public const int EXCHANGE_CODE = 19;
+        public const int GUIDE_RATING = 20;
+        public const int SHARE = 21;
+        public const int MCOIN = 22;
+        public const int BATTLE_PASS_RECHARGE = 23;
+        public const int ACHIEVEMENT = 24;
+        public const int PHOTOGRAPH = 25;
+        public const int NETWORK_LATENCY_ICON = 26;
+        public const int USER_CENTER = 27;
+        public const int ACCOUNT_BINDING = 28;
+        public const int RECOMMEND_PANEL = 29;
+        public const int CODEX = 30;
+        public const int REPORT = 31;
+        public const int DERIVATIVE_MALL = 32;
+        public const int EDIT_NAME = 33;
+        public const int EDIT_SIGNATURE = 34;
+        public const int RESIN_CARD = 35;
+        public const int FILE_INTEGRITY_CHECK = 36;
+        public const int ACTIVITY_H5 = 37;
+        public const int SURVEY = 38;
+        public const int CONCERT_PACKAGE = 39;
+        public const int PLAYER_PROFILE_CLOUD_GAME = 40;
+        public const int BATTLE_PASS_DISCOUNT_PRICE = 41;
+
+        public const int SHARE_SINA_WEIBO = 101;
+        public const int SHARE_QQ_FRIENDS = 102;
+        public const int SHARE_WECHAT_FRIENDS = 103;
+        public const int SHARE_TWITTER = 104;
+        public const int SHARE_KAKAO_STORY = 105;
+        public const int SHARE_QQ_ZONE = 106;
+        public const int SHARE_WECHAT_TIMELINE = 107;
+        public const int SHARE_FACEBOOK = 108;
+        public const int SHARE_INSTAGRAM = 109;
+        public const int SHARE_WHATSAPP = 110;
+        public const int SHARE_MESSENGER = 111;
+        public const int SHARE_MiHoYoBBS = 113;
+        public const int SHARE_HOYOLAB = 116;
+        public const int SHARE_BILIBILI = 120;
+        public const int SHARE_DOUYIN = 121;
+    }
+}
